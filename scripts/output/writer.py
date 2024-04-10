392c732a1670fdbd0ba109d6bf6e54318184a77f
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2024-04-10 09:01:05 +0200

    Add headers to the output files

--- scripts/output/writer.py
+++ scripts/output/writer.py
@@ -22,10 +22,12 @@ class ResultActivityHandler(ActivityHandler):
                                       approx_results: List[float], query_results: List[float]):
         path = os.path.join(self._path, f'{model.name()}-vi-{tool.__class__.__name__}.dat')
         with open(path, 'w') as file:
+            file.write('epsilon approx query\n')
             for row in zip(epsilons, approx_results, query_results):
                 file.write(f'{" ".join(map(str, row))}\n')
 
     def handle_linear_program_result(self, tool: Tool, model: Model, approx_result: float, query_result: float):
         path = os.path.join(self._path, f'{model.name()}-lp-{tool.__class__.__name__}.dat')
         with open(path, 'w') as file:
+            file.write('approx query\n')
             file.write(f'{approx_result} {query_result}')
